4e96a67fae70688076e6ae926f7b1d8460cd4812
Kimberly Olson <kolson21@cnm.edu>
2023-05-11 14:50:08 -0600

    milestone 3 check in point

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,3 +1,4 @@
+// enable email sending
 function sendEmail(){
     Email.send({
         SecureToken : "8e0eb6a3-f301-494d-a923-de46485e32c6",
@@ -9,3 +10,14 @@ function sendEmail(){
         message => alert("message was sent")
     )
 }
+
+// enable dark mode
+// let dark = document.getElementById("dark");
+// dark.onclick = function() {
+//     document.body.classList.toggle("dark-mode")
+//         if(document.body.classList.contains("dark-mode")){
+//             dark.src = "<i class=\"uil uil-sun\"></i>"
+//         }else{
+//             dark.src = "<i class=\"uil uil-moon\"></i>"
+//         }
+// }
