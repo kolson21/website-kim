4e2772639141d5a8e3c212f64b65db916a26ac52
Kimberly Olson <kolson21@cnm.edu>
2023-05-19 00:27:10 -0600

    ok

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -25,3 +25,15 @@ function sendEmail(){
 //             dark.src = "<i class=\"uil uil-moon\"></i>"
 //         }
 // }
+
+window.onscroll = function() {myFunction()};
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
